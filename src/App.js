173813ca065aa9f8a85ddf735f1a60b88edbc51a
Achour Mohamed El Bachir <achour.med.bachir@gmail.com>
2022-04-29 18:12:00 +0100

    broken our homepage into smaller compoents, finished styling our menu-item component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React  from 'react';
 import './App.css';
 
-import HomePage from './homepage.component';
+import HomePage from './pages/homepage/homepage.component';
+import Directory from './components/directory/directory.component';
+
 
 function App(){
   return <div>
